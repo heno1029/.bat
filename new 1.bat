839fb14ac0b46d13f5eec6538d0f3d4afa42abae
Almas-Ali <57622296+Almas-Ali@users.noreply.github.com>
2020-10-01 11:43:11 +0600

    Update new 1.bat

--- new 1.bat
+++ new 1.bat
@@ -1,17 +1,20 @@
 @echo off
 color 57
+
 echo Hey, do you love me (only answer in yes or no)
-set /p love=
-if %love%==yes goto love
-if %love%==no goto hate
+set /p love=choice:
+if /i "%love%"=="yes" (goto :love)
+if /i "%love%"=="no" (goto :hate)
+
 :love
 echo I love you too...
 echo Meet you soon :)
 pause 
-exit
+exit /b
+
 :hate
 echo But I love you....hehehehehe
 echo You are hacked...
 echo Your PC will crash in 10 seconds
-timeout 10
-shutdown -s -t 100
+timeout 10 <nul
+shutdown -s -t 0
