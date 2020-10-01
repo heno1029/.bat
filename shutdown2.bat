fe2724be017382fedc34d8a2c60727761878a3a5
Almas-Ali <57622296+Almas-Ali@users.noreply.github.com>
2020-10-01 11:47:27 +0600

    Update shutdown2.bat

--- shutdown2.bat
+++ shutdown2.bat
@@ -1,16 +1,19 @@
 @echo off
 color 57
+
 echo Do you want your computer to shutdown (only answer in yes or no)
-set /p love=
-if %love%==yes goto Exit
-if %love%==no goto Shutdown
+set /p love=choice:
+if "%love%"=="yes" (goto :Exit)
+if "%love%"=="no" (goto :Shutdown)
+
 :Exit
 echo You will not be shutdown
 echo Hooray!!:)
 pause 
-exit
+exit /b
+
 :Shutdown
 echo You will be shutdown
 echo Your PC will crash in 10 seconds
-timeout 10
-shutdown -s -t 100
+timeout 10 <nul
+shutdown -s -t 0
